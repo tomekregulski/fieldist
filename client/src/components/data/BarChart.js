ddba4e72a28d8d07da8cc768db188f272ee8dd43
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-07-12 13:58:04 -0700

    DataVisualization component now splits data object into individual objects for each data type to be rendered by charts.

--- client/src/components/data/BarChart.js
+++ client/src/components/data/BarChart.js
@@ -1,123 +1,5 @@
 import React from "react";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-  const reportData = [
-        {
-            "id": 1,
-            "sales": 15,
-            "interactions": 60,
-            "overall": 4,
-            "comments": "This was a great event"
-        },
-        {
-            "id": 2,
-            "sales": 14,
-            "interactions": 60,
-            "overall": 4,
-            "comments": "This was a great event"
-        },
-        {
-            "id": 3,
-            "sales": 25,
-            "interactions": 60,
-            "overall": 5,
-            "comments": "This was a great event"
-        },
-        {
-            "id": 4,
-            "sales": 23,
-            "interactions": 60,
-            "overall": 5,
-            "comments": "This was a great event"
-        },
-        {
-            "id": 5,
-            "sales": 13,
-            "interactions": 60,
-            "overall": 3,
-            "comments": "This was a great event"
-        },
-        {
-            "id": 6,
-            "sales": 18,
-            "interactions": 60,
-            "overall": 4,
-            "comments": "This was a great event"
-        },
-    ];
-
-  const salesData = [
-        {
-            "name": 1,
-            "value": 15
-        },
-        {
-            "name": 2,
-            "value": 14
-        },
-        {
-            "name": 3,
-            "value": 25
-        },
-        {
-            "name": 4,
-            "value": 23
-        },
-        {
-            "name": 5,
-            "value": 13
-        },
-        {
-            "name": 6,
-            "value": 18
-        },
-    ];
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 class BarRechartComponent extends React.Component {
 
     render() {
@@ -126,7 +8,7 @@ class BarRechartComponent extends React.Component {
         <BarChart
           width={500}
           height={300}
-          data={salesData}
+          data={this.props.data}
           margin={{
             top: 5,
             right: 30,
@@ -139,7 +21,7 @@ class BarRechartComponent extends React.Component {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="value" fill="#82ca9d" />
+          <Bar dataKey='value' fill="#82ca9d" />
         </BarChart>
       </ResponsiveContainer>
     );
--- client/src/components/pages/data/DataVisualization.js
+++ client/src/components/pages/data/DataVisualization.js
@@ -1,94 +1,108 @@
 import React, { useState, useEffect } from "react";
-import PieRechartComponent from "../../data/PieChart"
+// import PieRechartComponent from "../../data/PieChart"
 import BarRechartComponent from "../../data/BarChart"
 import authHeader from '../../../services/auth-header';
 
 function Data() {
   const [data, setData] = useState([]);
+  const [sales, setSales] = useState([]);
+  const [interactions, setInteractions] = useState([]);
+  const [overall, setOverall] = useState([]);
 
-  const salesData = [
+  const reportData = [
         {
-            "name": 1,
-            "value": 15
+            "id": 1,
+            "sales": 15,
+            "interactions": 65,
+            "overall": 4,
+            "comments": "This was a great event"
         },
         {
-            "name": 2,
-            "value": 14
+            "id": 2,
+            "sales": 14,
+            "interactions": 68,
+            "overall": 4,
+            "comments": "This was a great event"
         },
         {
-            "name": 3,
-            "value": 25
+            "id": 3,
+            "sales": 25,
+            "interactions": 80,
+            "overall": 5,
+            "comments": "This was a great event"
         },
         {
-            "name": 4,
-            "value": 23
+            "id": 4,
+            "sales": 23,
+            "interactions": 46,
+            "overall": 5,
+            "comments": "This was a great event"
         },
         {
-            "name": 5,
-            "value": 13
+            "id": 5,
+            "sales": 13,
+            "interactions": 50,
+            "overall": 3,
+            "comments": "This was a great event"
         },
         {
-            "name": 6,
-            "value": 18
+            "id": 6,
+            "sales": 18,
+            "interactions": 40,
+            "overall": 4,
+            "comments": "This was a great event"
         },
-    ]
-
-  const interactionsData = [
-        {
-            "name": 1,
-            "value": 65
-        },
-        {
-            "name": 2,
-            "value": 68
-        },
-        {
-            "name": 3,
-            "value": 80
-        },
-        {
-            "name": 4,
-            "value": 46
-        },
-        {
-            "name": 5,
-            "value": 50
-        },
-        {
-            "name": 6,
-            "value": 40
-        },
-    ]
+    ];
   
-  const myRequest = 'hello';
-  // new Request('/api/demos', {
+  // const myRequest = new Request('/api/data', {
   //   method: 'GET',
   //   headers: authHeader(),
   //   mode: 'cors',
   //   cache: 'default',
   // });
 
-    useEffect(() => {
-      setData('hello');
+  useEffect(() => {
+    setData(reportData);
     // fetch(myRequest)
     //   .then((res) => res.json())
     //   .then((response) => setData(response.map((res) => res)))
     //   .catch((err) => console.log(err));
+    setSales(data.map((item) => { 
+    return {
+      id: item.id,
+      value: item.sales
+    };
+   }));
+    setInteractions(data.map((item) => { 
+    return {
+      id: item.id,
+      value: item.interactions
+    };
+   }));
+    setOverall(data.map((item) => { 
+    return {
+      id: item.id,
+      value: item.overall
+    };
+   }));
   }, []);
 
-  console.log(data);
+  console.log(sales);
+  console.log(interactions);
+  console.log(overall);
 
   return (
     <div>
         <h1>Data Dashboard</h1>
           <div className="d-flex flex-column align-items-center">
-            <div className="d-flex mt-5">
+            {/* <div className="d-flex mt-5">
               <PieRechartComponent data={salesData} />
               <PieRechartComponent data={interactionsData} />
-            </div>
+            </div> */}
             <div className="d-flex mt-5">
-              <BarRechartComponent />
-              <BarRechartComponent />
+              <BarRechartComponent data={sales} />
+              <BarRechartComponent data={interactions} />
+              <BarRechartComponent data={overall} />
             </div>
           </div>
     </div>
